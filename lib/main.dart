3f2f974782b0813e3fb28ee7729fc7582d2fda9e
aqsal <muhammadaqsal96@gmail.com>
2022-12-21 11:39:57 +0700

    Aqsal Ganteng Update Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:js';
+import 'package:apps_kelolauang/screens/create_screen.dart';
+import 'package:apps_kelolauang/screens/update_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,35 +19,89 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  showAlertDialog(BuildContext context) {
+    Widget okButton = TextButton(
+      child: Text("Yakin"),
+      onPressed: () {},
+    );
+
+    AlertDialog alertDialog = AlertDialog(
+      title: Text("Peringatan !"),
+      content: Text("Anda yakin akan menghapus ?"),
+      actions: [okButton],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Kelola Duit App"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => CreateScreen()));
+            },
+          )
+        ],
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text("Total Uang Masuk: Rp. 1000"),
-            SizedBox(height: 20,),
-            Text("Total Uang Keluar: Rp.500"),
-            ListTile(title: Text("Lunch"),
-            subtitle: Text("Rp.200"),
-            leading: Icon(
-            Icons.download, 
-            color: Colors.green,
-            ),
-            trailing: Wrap(
-              children: [
-                Icon(Icons.edit, color: Colors.black,),
-                SizedBox(width: 20,),
-                Icon(Icons.delete, color: Colors.red,),
+          child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Text("Total Uang Masuk: Rp. 1000"),
+          SizedBox(
+            height: 20,
+          ),
+          Text("Total Uang Keluar: Rp.500"),
+          ListTile(
+              title: Text("Lunch"),
+              subtitle: Text("Rp.200"),
+              leading: Icon(
+                Icons.download,
+                color: Colors.green,
+              ),
+              trailing: Wrap(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => UpdateScreen()));
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                        color: Colors.grey,
+                      )),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showAlertDialog(context);
+                      },
+                      icon: Icon(Icons.delete, color: Colors.red))
                 ],
-            ) 
-            )
-          ],
-        )),
+              ))
+        ],
+      )),
     );
   }
 }
-
