9c76b41ce72adea4b365c253b92511b2ff4493ef
mrahmatmuhaimin <muhaiminrahmat@gmail.com>
2022-12-22 17:31:32 +0700

    Update and Finishing Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:js';
 import 'package:apps_kelolauang/screens/create_screen.dart';
 import 'package:apps_kelolauang/screens/update_screen.dart';
+import 'package:apps_kelolauang/db/database_instance.dart';
+import 'package:apps_kelolauang/models/transaksi_model.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,29 +9,52 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Kelola Keuangan",
+      title: "Kelola Duitku",
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  showAlertDialog(BuildContext context) {
+  DatabaseInstance? databaseInstance;
+
+  Future _refresh() async {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    databaseInstance = DatabaseInstance();
+    initDatabase();
+    super.initState();
+  }
+
+  Future initDatabase() async {
+    await databaseInstance!.database();
+    setState(() {});
+  }
+
+  showAlertDialog(BuildContext contex, int idTransaksi) {
     Widget okButton = TextButton(
       child: Text("Yakin"),
-      onPressed: () {},
+      onPressed: () {
+        //delete disini
+        databaseInstance!.hapus(idTransaksi);
+        Navigator.of(contex, rootNavigator: true).pop();
+        setState(() {});
+      },
     );
 
     AlertDialog alertDialog = AlertDialog(
@@ -40,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     showDialog(
-        context: context,
+        context: contex,
         builder: (BuildContext context) {
           return alertDialog;
         });
@@ -50,58 +74,126 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Kelola Duit App"),
+        title: Text("Home Page"),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => CreateScreen()));
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => CreateScreen()))
+                  .then((value) {
+                setState(() {});
+              });
             },
           )
         ],
       ),
-      body: SafeArea(
-          child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Text("Total Uang Masuk: Rp. 1000"),
-          SizedBox(
-            height: 20,
-          ),
-          Text("Total Uang Keluar: Rp.500"),
-          ListTile(
-              title: Text("Lunch"),
-              subtitle: Text("Rp.200"),
-              leading: Icon(
-                Icons.download,
-                color: Colors.green,
-              ),
-              trailing: Wrap(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => UpdateScreen()));
-                      },
-                      icon: Icon(
-                        Icons.edit,
-                        color: Colors.grey,
-                      )),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        showAlertDialog(context);
-                      },
-                      icon: Icon(Icons.delete, color: Colors.red))
-                ],
-              ))
-        ],
-      )),
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: SafeArea(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            FutureBuilder(
+                future: databaseInstance!.totalPemasukan(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("-");
+                  } else {
+                    if (snapshot.hasData) {
+                      return Text(
+                          "Total pemasukan : Rp. ${snapshot.data.toString()}");
+                    } else {
+                      return Text("");
+                    }
+                  }
+                }),
+            SizedBox(
+              height: 20,
+            ),
+            FutureBuilder(
+                future: databaseInstance!.totalPengeluaran(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("-");
+                  } else {
+                    if (snapshot.hasData) {
+                      return Text(
+                          "Total pengeluaran : Rp. ${snapshot.data.toString()}");
+                    } else {
+                      return Text("");
+                    }
+                  }
+                }),
+            FutureBuilder<List<TransaksiModel>>(
+                future: databaseInstance!.getAll(),
+                builder: (context, snapshot) {
+                  print('HASIL : ' + snapshot.data.toString());
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("Loading");
+                  } else {
+                    if (snapshot.hasData) {
+                      return Expanded(
+                        child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                  title: Text(snapshot.data![index].name!),
+                                  subtitle: Text(
+                                      snapshot.data![index].total!.toString()),
+                                  leading: snapshot.data![index].type == 1
+                                      ? Icon(
+                                          Icons.download,
+                                          color: Colors.green,
+                                        )
+                                      : Icon(
+                                          Icons.upload,
+                                          color: Colors.red,
+                                        ),
+                                  trailing: Wrap(
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .push(MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        UpdateScreen(
+                                                          transaksiMmodel:
+                                                              snapshot
+                                                                  .data![index],
+                                                        )))
+                                                .then((value) {
+                                              setState(() {});
+                                            });
+                                          },
+                                          icon: Icon(
+                                            Icons.edit,
+                                            color: Colors.grey,
+                                          )),
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      IconButton(
+                                          onPressed: () {
+                                            showAlertDialog(context,
+                                                snapshot.data![index].id!);
+                                          },
+                                          icon: Icon(Icons.delete,
+                                              color: Colors.red))
+                                    ],
+                                  ));
+                            }),
+                      );
+                    } else {
+                      return Text("Tidak ada data");
+                    }
+                  }
+                })
+          ],
+        )),
+      ),
     );
   }
 }
--- lib/screens/create_screen.dart
+++ lib/screens/create_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:apps_kelolauang/db/database_instance.dart';
 
-class CreateScreen extends StatelessWidget {
+class CreateScreen extends StatefulWidget {
   const CreateScreen({Key? key}) : super(key: key);
 
-  final int _value = 1;
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  DatabaseInstance databaseInstance = DatabaseInstance();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController totalController = TextEditingController();
+  int _value = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    databaseInstance.database();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,30 +36,58 @@ class CreateScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text("Nama"),
-            TextField(),
+            TextField(
+              controller: nameController,
+            ),
             SizedBox(
               height: 20,
             ),
             Text("Tipe Transaksi"),
             ListTile(
               title: Text("Pemasukan"),
-              leading:
-                  Radio(groupValue: _value, value: 1, onChanged: (value) {}),
+              leading: Radio(
+                  groupValue: _value,
+                  value: 1,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = int.parse(value.toString());
+                    });
+                  }),
             ),
             ListTile(
               title: Text("Pengeluaran"),
-              leading:
-                  Radio(groupValue: _value, value: 2, onChanged: (value) {}),
+              leading: Radio(
+                  groupValue: _value,
+                  value: 2,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = int.parse(value.toString());
+                    });
+                  }),
             ),
             SizedBox(
               height: 20,
             ),
             Text("Total"),
-            TextField(),
+            TextField(
+              controller: totalController,
+            ),
             SizedBox(
               height: 30,
             ),
-            ElevatedButton(onPressed: () {}, child: Text("Simpan")),
+            ElevatedButton(
+                onPressed: () async {
+                  int idInsert = await databaseInstance.insert({
+                    'name': nameController.text,
+                    'type': _value,
+                    'total': totalController.text,
+                    'created_at': DateTime.now().toString(),
+                    'updated_at': DateTime.now().toString(),
+                  });
+                  print("sudah masuk : " + idInsert.toString());
+                  Navigator.pop(context);
+                },
+                child: Text("Simpan")),
           ],
         ),
       )),
